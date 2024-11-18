c3694b64e7a51d68aa247e684685abfd7948a317
Kuro-no.Kenshi <dani04ranzo@gmail.com>
2024-11-18 15:07:08 +0100

    changed app name and icon

--- game_planner_mobile/lib/ui/widgets/animated_fantasy_text.dart
+++ game_planner_mobile/lib/ui/widgets/animated_fantasy_text.dart
@@ -4,6 +4,7 @@ class AnimatedFantasyText extends StatefulWidget {
   const AnimatedFantasyText({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _AnimatedFantasyTextState createState() => _AnimatedFantasyTextState();
 }
 
